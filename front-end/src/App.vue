7d253862fb7f65ddb698dc4a1e9298031b0312ae
Keaton Sentak <k.sentak@outlook.com>
2021-02-16 22:48:30 -0500

    move to vue 3 front-end

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
-      <router-link to="/register">Register</router-link> |
-      <router-link to="/login">Login</router-link>
-    </div>
-    <router-view />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
 <style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,11 +1,5 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
-import Login from '../views/Login.vue';
-import Register from '../views/Register.vue';
-
-Vue.use(VueRouter);
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
@@ -16,24 +10,16 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-];
+]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHashHistory(),
   routes
-});
+})
 
-export default router;
+export default router
